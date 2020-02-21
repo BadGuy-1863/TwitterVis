3f6c0d39ac4020673c436671a4bbcc950eb7e26a
BadGuy-1863 <56260120+BadGuy-1863@users.noreply.github.com>
2020-02-21 13:01:21 -0500

    First Commit

--- src/HelloTweepy.py
+++ src/HelloTweepy.py
@@ -1,3 +1,11 @@
 import tweepy
+c_key = "0iyK7MB6jiQ1KM4267bhOyNNk"
+c_sec = "bynOPvxzK74YjJtbXknhkE26MO2EIIbm86tiPegidIODYNsbP0"
 
-aut = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(c_key, c_sec)
+auth.set_access_token("1106421196642492417-qOfYHTZOFhi3pmSeo3UsQWxyknKDcn", "GAS8iUymH0EL61WqhogBFyPeZuUKxvF1YHW1svAri3DkS")
+
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
